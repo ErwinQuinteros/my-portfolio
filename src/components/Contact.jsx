53f8026eb4ef10d66fca669ec9d0f6612efe70ee
Erwin Quinteros <erwinquinterosvillarroel@gmail.com>
2024-10-09 20:06:00 -0300

    update - portfolio contact funcionality

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,18 +1,67 @@
-import React from "react";
+import emailjs from "emailjs-com";
 import { BiLogoLinkedin } from "react-icons/bi";
 import { IoLocationSharp } from "react-icons/io5";
 import { FaFacebook, FaStackOverflow } from "react-icons/fa";
 import { FaXTwitter } from "react-icons/fa6";
 import { IoLogoGithub, IoMdCall } from "react-icons/io";
 import { MdAlternateEmail } from "react-icons/md";
+import swal from "sweetalert";
+import "./../index.css";
 
 const Contact = () => {
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    const { name, email, message } = e.target.elements;
+
+    const templateParams = {
+      from_name: name.value,
+      email: email.value,
+      message: message.value,
+    };
+
+    emailjs.send(
+        "service_3hi86ab",
+        "template_8twxt8d",
+        templateParams,
+        "ystOkJA7-d_DAwXUz"
+      )
+      .then(
+        (response) => {
+          console.log(
+            "Email successfully sent!",
+            response.status,
+            response.text
+          );
+          swal({
+            title: "¡Éxito!",
+            text: "Tu mensaje ha sido enviado con éxito.",
+            icon: "success",
+            className: "my-swal-success",
+            buttons: false,
+            timer: 3000,
+          });
+        },
+        (err) => {
+          console.error("Failed to send email. Error:", err);
+          swal({
+            title: "Error",
+            text: "Hubo un problema al enviar tu mensaje. Inténtalo de nuevo.",
+            icon: "error",
+            className: "my-swal-error",
+            buttons: false,
+            timer: 3000,
+          });
+        }
+      );
+  };
+
   return (
     <section
       id="about-us"
       className="relative z-50 bg-[#030618] border-[#353951] border-t"
     >
-       <div className="py-10 px-6 mx-auto sm:px-16 sm:py-16 lg:max-w-[70rem] xl:max-w-[76rem] 2xl:max-w-[80rem]">
+      <div className="py-10 px-6 mx-auto sm:px-16 sm:py-16 lg:max-w-[70rem] xl:max-w-[76rem] 2xl:max-w-[80rem]">
         <div className="lg:w-[80%] text-white text-left mb-5">
           <h1 className="font-black sm:text-[40px] text-[30px] lg:w-[85%]">
             Contacto
@@ -22,10 +71,9 @@ const Contact = () => {
           <div className="max-w-3xl text-white rounded-lg border border-[#464c6a] p-3 lg:p-5">
             <p className="text-left text-md text-[#d3d8e8] py-3">
               Si tiene alguna pregunta o inquietud, no dude en ponerse en
-              contacto conmigo. Estoy abierto a cualquier oportunidad de trabajo
-              que se alinee con mis habilidades e intereses.
+              contacto conmigo.
             </p>
-            <form>
+            <form onSubmit={handleSubmit}>
               <div className="mb-4">
                 <label
                   htmlFor="name"
@@ -34,6 +82,7 @@ const Contact = () => {
                   Nombre:
                 </label>
                 <input
+                  required
                   type="text"
                   id="name"
                   className="w-full p-2 text-white bg-gray-800 border border-gray-700 rounded"
@@ -47,6 +96,7 @@ const Contact = () => {
                   Email:
                 </label>
                 <input
+                  required
                   type="email"
                   id="email"
                   className="w-full p-2 text-white bg-gray-800 border border-gray-700 rounded"
@@ -60,6 +110,7 @@ const Contact = () => {
                   Mensaje:
                 </label>
                 <textarea
+                  required
                   id="message"
                   rows="4"
                   className="w-full p-2 text-white bg-gray-800 border border-gray-700 rounded"
