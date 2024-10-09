c2cc4ce29c59d3d554d70af50a42e8b6fd108a68
Erwin Quinteros <erwinquinterosvillarroel@gmail.com>
2024-10-08 21:50:50 -0300

    advanced - section experience

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,49 +1,56 @@
 import React from "react";
+import "./../index.css";
 const About = () => {
   return (
-    <section id="about-us" className="relative z-50 bg-white">
+    <section
+      id="about-us"
+      className="relative z-50 bg-[#030618] border-[#353951] border-t"
+    >
       <div className="py-10 px-6 mx-auto sm:px-16 sm:py-16 lg:max-w-[70rem] xl:max-w-[76rem] 2xl:max-w-[92rem]">
-        <div className="self-center">
-          <div className="flex flex-wrap gap-2 pt-5 align-center md:flex-nowrap">
-            <div className="lg:w-[80%] text-gray-900 text-left">
-              <h2 className="text-xl">Software Developer</h2>
-              <h1 className="font-black mt-3 text-5xl lg:w-[85%]">Resumen</h1>
-              <p className="mt-6 text-base lg:w-[87%] leading-7 text-justify font-normal">
-                ¡Hola! Soy Erwin Quinteros, un apasionado desarrollador de
-                software con experiencia en backend y frontend. A lo largo de mi
-                carrera, he tenido la oportunidad de trabajar en una amplia
-                variedad de proyectos, desde aplicaciones web complejas hasta
-                soluciones de comercio electrónico. Mi enfoque siempre ha sido
-                ofrecer productos de alta calidad que no solo cumplan con los
-                requisitos técnicos, sino que también proporcionen una
-                experiencia de usuario excepcional.
-              </p>
-              <div className="flex gap-2 mt-6">
-                <a
-                  className="flex items-center gap-2 py-3 text-[#030618] transition border-2 font-bold border-black rounded-full outline-none group bg-white px-5 focus:scale-105 hover:scale-105 active:scale-105"
-                  href="#projects"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  <span className="opacity-70">Proyectos</span>
-                </a>
-                <a
-                  className="flex items-center gap-2 py-3 text-[#030618] transition border-2 font-bold border-black rounded-full outline-none group bg-white px-5 focus:scale-105 hover:scale-105 active:scale-105"
-                  href="#contact"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  <span className="opacity-70">Contacto</span>
-                </a>
-              </div>
+        <div className="flex justify-center -z-40">
+          <div className="absolute top-0 h-[1px] w-1/2  bg-gradient-to-r from-transparent via-red-500 to-transparent"></div>
+        </div>
+        <div className="flex flex-wrap gap-2 pt-5 align-center md:flex-nowrap">
+          <div className="lg:w-[80%] text-white text-left">
+            <h2 className="sm:text-[18px] text-[16px]">Software Developer</h2>
+            <h1 className="font-black sm:text-[40px] text-[30px] lg:w-[85%]">
+              Resumen
+            </h1>
+            <p className="mt-6 text-base lg:w-[87%] leading-7 text-justify font-normal">
+              ¡Hola! Soy Erwin Quinteros, un apasionado desarrollador de
+              software con experiencia en backend y frontend. A lo largo de mi
+              carrera, he tenido la oportunidad de trabajar en una amplia
+              variedad de proyectos, desde aplicaciones web complejas hasta
+              soluciones de comercio electrónico. Mi enfoque siempre ha sido
+              ofrecer productos de alta calidad que no solo cumplan con los
+              requisitos técnicos, sino que también proporcionen una experiencia
+              de usuario excepcional.
+            </p>
+            <div className="flex gap-2 mt-6">
+              <a
+                className="flex items-center gap-2 py-3 text-white transition border-2 font-bold border-white rounded-full outline-none group bg-[#030618] px-5 focus:scale-105 hover:scale-105 active:scale-105"
+                href="#projects"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <span className="opacity-70">Proyectos</span>
+              </a>
+              <a
+                className="flex items-center gap-2 py-3 text-white transition border-2 font-bold border-white rounded-full outline-none group bg-[#030618] px-5 focus:scale-105 hover:scale-105 active:scale-105"
+                href="#contact"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <span className="opacity-70">Contacto</span>
+              </a>
             </div>
+          </div>
 
-            <div className="hidden lg:block w-[480px] self-center">
-              <img
-                className="rounded-[10%] transform"
-                src="https://images.unsplash.com/photo-1531973576160-7125cd663d86"
-              />
-            </div>
+          <div className="hidden lg:block w-[480px] self-center">
+            <img
+              className="rounded-[10%] transform"
+              src="https://images.unsplash.com/photo-1531973576160-7125cd663d86"
+            />
           </div>
         </div>
       </div>
--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,84 +1,104 @@
 import React from "react";
-import { motion } from "framer-motion";
-import { textVariant } from "../utils/motion";
-import { styles } from "../styles";
 import {
   VerticalTimeline,
   VerticalTimelineElement,
 } from "react-vertical-timeline-component";
 import "react-vertical-timeline-component/style.min.css";
-import { FaRegQuestionCircle, FaServer, FaLaptopCode } from "react-icons/fa";
+import { FaServer, FaLaptopCode } from "react-icons/fa";
+import { VscWorkspaceTrusted } from "react-icons/vsc";
+import { color } from "framer-motion";
+import { BiFontSize } from "react-icons/bi";
 
 const Experience = () => {
   return (
-    <div id="experience" className="py-6 bg-white">
-     <motion.div variants={textVariant()}>
-        <p className={`${styles.sectionSubText} text-center`}>
-          Lo que he hecho hasta ahora
-        </p>
-        <h2 className={`${styles.sectionHeadText} text-center`}>
-          Experiencia Laboral.
-        </h2>
-      </motion.div>
-      <div className='mt-20 mb-20'>
-      <VerticalTimeline>
-        <VerticalTimelineElement
-          contentStyle={{
-            background: "#262D3A",
-            color: "#fff",
-            marginTop: "2rem"
-          }}
-          date="2020-08-09"
-          iconStyle={{ background: "#777777", color: "#333845" }}
-          icon={<FaRegQuestionCircle />}
-        >
-          <div className="text-left">
-            <h3 className="text-lg font-bold">QA (Quality Assurance)</h3>
-            <h4 className="font-bold text-md">Xanthops SRL</h4>
-            <p className="text-gray-600">
-              Description of QA responsibilities and experiences.
-            </p>
-          </div>
-        </VerticalTimelineElement>
-
-        <VerticalTimelineElement
-          contentStyle={{
-            background: "#262D3A",
-            color: "#fff",
-          }}
-          date="2024-07-20"
-          iconStyle={{ background: "#777777", color: "#fff" }}
-          icon={<FaServer />}
-        >
-          <div className="text-left">
-            <h3 className="text-lg font-bold">QA (Quality Assurance)</h3>
-            <h4 className="font-bold text-md">Xanthops SRL</h4>
-            <p className="text-gray-600">
-              Description of QA responsibilities and experiences.
-            </p>
-          </div>
-        </VerticalTimelineElement>
-
-        <VerticalTimelineElement
-          contentStyle={{
-            background: "#262D3A",
-            color: "#fff",
-          }}
-          date="2024-06-15"
-          iconStyle={{ background: "#777777", color: "#fff" }}
-          icon={<FaLaptopCode />}
-        >
-          <div className="text-left">
-            <h3 className="text-lg font-bold">QA (Quality Assurance)</h3>
-            <h4 className="font-bold text-md">Xanthops SRL</h4>
-            <p className="text-gray-600">
-              Description of QA responsibilities and experiences.
-            </p>
-          </div>
-        </VerticalTimelineElement>
-      </VerticalTimeline>
+    <section
+      id="experience"
+      className="relative z-50 bg-[#030618] border-[#353951] border-t"
+    >
+      <div className="flex justify-center -z-40">
+        <div className="absolute top-0 h-[1px] w-1/2  bg-gradient-to-r from-transparent via-red-500 to-transparent"></div>
       </div>
-    </div>
+      <div className="py-10 px-6 mx-auto sm:px-16 sm:py-16 lg:max-w-[70rem] xl:max-w-[76rem] 2xl:max-w-[92rem]">
+        <div>
+          <p className="sm:text-[18px] text-[16px] text-white tracking-wider text-center">
+            Lo que he hecho hasta ahora
+          </p>
+          <h2 className="text-white font-black sm:text-[40px] text-[30px]">
+            Experiencia Laboral
+          </h2>
+        </div>
+        <div className="sm:mt-11 sm:mb-11 md:mb-16 md:mt-16">
+          <VerticalTimeline
+            contentStyle={{ borderRight: "7px solid rgb(33, 150, 243)" }}
+            className="text-black"
+          >
+            <VerticalTimelineElement
+              className="text-start"
+              contentStyle={{
+                background: "#fff",
+                color: "#111827",
+                marginTop: "2rem",
+              }}
+              dateClassName="text-white"
+              date="2020-08-09"
+              iconStyle={{
+                background: "#030618",
+                color: "#fff",
+                BiFontSize: 2,
+              }}
+              icon={<VscWorkspaceTrusted />}
+            >
+              <div className="text-center">
+                <h3 className="text-lg font-bold">QA (Quality Assurance)</h3>
+                <h4 className="font-bold text-md">Xanthops SRL</h4>
+                <p className="text-gray-900">
+                  Description of QA responsibilities and experiences.
+                </p>
+              </div>
+            </VerticalTimelineElement>
+
+            <VerticalTimelineElement
+              contentStyle={{
+                background: "#fff",
+                color: "#111827",
+              }}
+              dateClassName="text-white"
+              date="2024-07-20"
+              iconStyle={{ background: "#030618", color: "#fff" }}
+              icon={<FaServer />}
+            >
+              <div className="text-left">
+                <h3 className="text-lg font-bold">QA (Quality Assurance)</h3>
+                <h4 className="font-bold text-md">Xanthops SRL</h4>
+                <p className="text-gray-600">
+                  Description of QA responsibilities and experiences.
+                </p>
+              </div>
+            </VerticalTimelineElement>
+
+            <VerticalTimelineElement
+              className="text-start"
+              contentStyle={{
+                background: "#fff",
+                color: "#111827",
+              }}
+              dateClassName="text-white"
+              date="2024-06-15"
+              iconStyle={{ background: "#030618", color: "#fff" }}
+              icon={<FaLaptopCode />}
+            >
+              <div className="text-left">
+                <h3 className="text-lg font-bold">QA (Quality Assurance)</h3>
+                <h4 className="font-bold text-md">Xanthops SRL</h4>
+                <p className="text-gray-600">
+                  Description of QA responsibilities and experiences.
+                </p>
+              </div>
+            </VerticalTimelineElement>
+          </VerticalTimeline>
+        </div>
+      </div>
+    </section>
   );
 };
 
